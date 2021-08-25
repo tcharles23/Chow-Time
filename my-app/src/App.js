7df29c8820fa6010cac6efe406c11ca81e99721e
Akshathak97 <88549019+Akshathak97@users.noreply.github.com>
2021-08-25 17:51:43 -0400

    Update App.js
    app.js for login page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import './util.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="limiter">
+        <div className="container-login100">
+          <div className="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+            <form className="login100-form validate-form flex-sb flex-w">
+            <span className="login100-form-title p-b-32">
+						Account Login
+					</span>
+
+					<span className="txt1 p-b-11">
+						Username
+					</span>
+
+          <div className="wrap-input100 validate-input m-b-36" data-validate = "Username is required">
+						<input className="input100" type="text" name="username" ></input>
+						<span className="focus-input100"></span>
+					</div>
+
+          <span className="txt1 p-b-11">
+						Password
+					</span>
+					<div className="wrap-input100 validate-input m-b-12" data-validate = "Password is required">
+						<span className="btn-show-pass">
+							<i className="fa fa-eye"></i>
+						</span>
+						<input className="input100" type="password" name="pass" ></input>
+						<span className="focus-input100"></span>
+					</div>
+
+
+          <div className="flex-sb-m w-full p-b-48">
+						<div className="contact100-form-checkbox">
+							<input className="input-checkbox100" id="ckb1" type="checkbox" name="remember-me"></input>
+							<label className="label-checkbox100" for="ckb1">
+								Remember me
+							</label>
+						</div>
+            <div>
+							<a href="index.html" class="txt3">
+								Forgot Password?
+							</a>
+						</div>
+					</div>
+
+          <div className="container-login100-form-btn">
+						<button className="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+
+
+
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </body>
   );
 }
 
